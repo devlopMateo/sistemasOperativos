26f728c828e09520bf5a41821fd9cf741287b9b5
lucas <lucassalna06gmail.com>
2025-05-21 21:33:21 -0500

    se adicionan los botones de google, bloc de notas, cmd

--- src/main/java/com/mycompany/lanzaapp/LanzaApp.java
+++ src/main/java/com/mycompany/lanzaapp/LanzaApp.java
@@ -20,17 +20,17 @@ public class LanzaApp {
     public static void main(String[] args) {
 
         //se crea una vista
-        JFrame frame = new JFrame("Abrir Calculadora");
+        JFrame frame = new JFrame("Shell Grafico personalizado");
         //con esta instacia creamos un boton con un tamaño definido
-        JButton button = new JButton("Calculadora");
-        button.setPreferredSize(new Dimension(110, 50));
+        JButton button1 = new JButton("Google");
+        button1.setPreferredSize(new Dimension(110, 50));
      
         //se agrega un escuchador de eventos lo que hara que se levante el aplicativo al dar click
-        button.addActionListener(new ActionListener() {
+        button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    ProcessBuilder processBuilder = new ProcessBuilder("calc.exe");
+                   ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c", "start https://www.google.com");
                     processBuilder.start();
                 } catch (Exception ex) {
                     JOptionPane.showMessageDialog(null, ex);
@@ -38,8 +38,44 @@ public class LanzaApp {
                 }
             }
         });
+        JButton button2 = new JButton("bloc de notas");
+        button2.setPreferredSize(new Dimension(110, 50));
+     
+        //se agrega un escuchador de eventos lo que hara que se levante el aplicativo al dar click
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    ProcessBuilder processBuilder = new ProcessBuilder("notepad");
+                    processBuilder.start();
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, ex);
+                    ex.printStackTrace();
+                }
+            }
+        });
+        
+        JButton button3 = new JButton("CMD");
+        button3.setPreferredSize(new Dimension(110, 50));
+     
+        //se agrega un escuchador de eventos lo que hara que se levante el aplicativo al dar click
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c", "start cmd");
+                    processBuilder.start();
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, ex);
+                    ex.printStackTrace();
+                }
+            }
+        });
+        
         frame.setLayout(new FlowLayout());
-        frame.add(button);
+        frame.add(button1);
+        frame.add(button2);
+        frame.add(button3);
         frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
